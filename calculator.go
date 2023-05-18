c101e90b271de77fbb9ea35d0d02c0079a4cb539
Filipe Lima <filipe@codesandbox.io>
2023-05-18 14:22:17 +0000

    Add quotient logic

--- calculator.go
+++ calculator.go
@@ -14,15 +14,21 @@ func multiply(x, y int) int {
 	return x * y
 }
 
+func divide(x, y int) int {
+	return x / y
+}
+
 func main() {
-	x := 10
-	y := 2
+	x := 40
+	y := 4
 
 	sum := add(x, y)
 	difference := subtract(x, y)
 	mult := multiply(x, y)
+	div := divide(x, y)
 
 	fmt.Println("Sum:", sum)
 	fmt.Println("Difference:", difference)
 	fmt.Println("Product:", mult)
+	fmt.Println("Quotient:", div)
 }
